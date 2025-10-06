c9c735ce96697605b82ef35a299dceada63df9c0
Will Ellis <william.elli5@icloud.com>
2025-10-06 17:33:00 +0100

    add comment correction

--- lib/verify_sentence.py
+++ lib/verify_sentence.py
@@ -7,3 +7,8 @@ def verify_sentence(text):
     if text[-1] in sentence_finishers:
         return text[0].upper()+text[1:]
     return text[0].upper()+text[1:]+'.'
+
+""" misinterpreted actual challenge - just wanted a boolean response, rather than full correction
+oh well, uses all the same principles, just slightly more complex than the actual challenge.
+obviously logic is some what flawed in real world context - cant detect tonality of sentence
+so always going to end with . as a pose to ever ending with ! or ? unless already supplied. """
